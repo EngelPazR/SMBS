71613e5ec75cf2c434750ee9e383d09e9b6b197f
Engel Josué Paz Rodríguez <engelpazrodriguez@gmail.com>
2022-07-04 12:54:29 -0600

    Un par de tareas que se repetian se metieron en una funcion para mandarlas a llamar solo donde se necesitaabn y asi crear un codigo más corto

--- css/js/To-Dolist1.js
+++ css/js/To-Dolist1.js
@@ -2,45 +2,8 @@ let boton = document.getElementById('agregar');
 let texto = document.getElementById('texto');
 let lista = document.getElementById('lista');
 let dbs = [];
-document.addEventListener('DOMContentLoaded', () => {
-  dbs = JSON.parse(localStorage.getItem('tareas')) || [];
-  lista.innerHTML = '';
 
-  dbs.forEach((elemento) => {
-    lista.innerHTML += `<section class="nuevo">
-    <span id='titulo'>
-    ${elemento}
-    </span>
- <button class="trash"><i class="fa-solid fa-trash-can"></i></button>
- </section>
- `;
-  });
-
-  let trash = document.querySelectorAll('.trash');
-  trash.forEach((elemento, i) => {
-    elemento.addEventListener('click', () => {
-      elemento.parentElement.remove();
-      console.log(i);
-      dbs.splice(i, 1);
-      let tareas = document.querySelectorAll('.nuevo');
-      console.log(dbs);
-      if (tareas.length === 0) {
-        dbs = [];
-      }
-      localStorage.setItem('tareas', JSON.stringify(dbs));
-    });
-  });
-});
-
-function addTask() {
-  if (texto.value === '') {
-    alert('Ingrese una tarea, por favor');
-    return;
-  }
-  let tarea = texto.value;
-  dbs.push(tarea);
-  console.log(dbs);
-
+function agregarTask() {
   lista.innerHTML = '';
   dbs.forEach((elemento) => {
     lista.innerHTML += `<section class="nuevo">
@@ -52,10 +15,10 @@ function addTask() {
  `;
     texto.value = '';
   });
+}
 
-  localStorage.setItem('tareas', JSON.stringify(dbs));
+function eliminarTask(params) {
   let trash = document.querySelectorAll('.trash');
-
   trash.forEach((elemento, i) => {
     elemento.addEventListener('click', () => {
       elemento.parentElement.remove();
@@ -69,6 +32,32 @@ function addTask() {
       localStorage.setItem('tareas', JSON.stringify(dbs));
     });
   });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  dbs = JSON.parse(localStorage.getItem('tareas')) || [];
+  lista.innerHTML = '';
+
+  agregarTask();
+
+  eliminarTask();
+});
+
+function addTask() {
+  if (texto.value === '') {
+    alert('Ingrese una tarea, por favor');
+    return;
+  }
+  let tarea = texto.value;
+  dbs.push(tarea);
+  console.log(dbs);
+
+  agregarTask();
+  texto.value = '';
+
+  localStorage.setItem('tareas', JSON.stringify(dbs));
+
+  eliminarTask();
 }
 
 boton.addEventListener('click', (e) => {
